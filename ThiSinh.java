1045bfdbf74fc37b72c80853f0252c7e4410ea3b
phantoai01 <86053227+phantoai01@users.noreply.github.com>
2021-08-27 17:15:18 +0700

    Update ThiSinh.java

--- ThiSinh.java
+++ ThiSinh.java
@@ -1,1 +1,41 @@
+import java.util.Scanner;
 
+public class ThiSinh {
+       private
+       String ht,dc,gt;
+       int ns;
+       
+       public ThiSinh(){
+
+       }
+       public ThiSinh(String a,int b,String c,String d)
+       {
+               this.ht = a;
+               this.ns = b;
+               this.dc = c;
+               this.gt = d; 
+       }
+       public void nhap(){
+               Scanner sc = new Scanner(System.in);
+               System.out.print("Nhap Ho Va Ten: ");
+               this.ht = sc.nextLine();    
+               System.out.print("Nhap Gioi Tinh: ");
+               this.gt = sc.nextLine();
+               System.out.print("Nhap Dia chi: ");
+               this.dc = sc.nextLine();
+               System.out.print("Nhap Nam Sinh: ");
+               this.ns = sc.nextInt();
+       }
+       public void xuat(){
+               System.out.println("\t\t\tThong Tin Thi Sinh da nhap la:");
+               System.out.println("Ho va Ten Thi Sinh: " + ht);
+               System.out.println("Gioi Tinh: " + gt);
+               System.out.println("Dia Chi: " + dc);
+               System.out.println("Nam Sinh: " + ns);
+       }
+       public static void main(String[] args) {
+               ThiSinh a = new ThiSinh();
+               a.nhap();
+               a.xuat();
+       }
+}
